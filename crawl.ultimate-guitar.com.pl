d41e7b2344f8f294632ed3175c26bdd5f29f7dc1
l veatch <5739666+lveatch@users.noreply.github.com>
2025-07-20 17:05:57 -0400

    Create crawl.ultimate-guitar.com.pl

---
+++ crawl.ultimate-guitar.com.pl
@@ -0,0 +1,158 @@
+#!/usr/bin/perl
+
+use LWP::UserAgent;
+use HTTP::Request;
+use URI::Heuristic;
+use HTML::Entities;
+use JSON;
+use JSON::MaybeXS qw(encode_json);
+use Data::Dumper;
+
+my $type = shift || die "enter type (chords, tabs, etc) for input.\n";
+$type = ucfirst($type);
+
+my $userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15';
+
+for my $pageNumber ( 1 .. 10_000) {
+   my $pageText = ($pageNumber > 1) ? "page=$pageNumber&" : '';
+
+   #my $raw_url = "https://www.ultimate-guitar.com/explore?order=songname_asc&${pageText}type[]=Tabs"; 
+   my $raw_url = "https://www.ultimate-guitar.com/explore?${pageText}type[]=$type"; 
+   print "getting page $pageNumber ... $raw_url \n";
+
+   my $url = URI::Heuristic::uf_urlstr($raw_url);
+   
+   my $ua = LWP::UserAgent->new();
+   $ua->agent($userAgent);
+   
+   $request = HTTP::Request->new(GET => $url);
+   $request->referer('https://www.ultimate-guitar.com/');
+   #print "request = $request\n";
+   
+   my $webRequest = $ua->request($request);
+   #print "web request = $webRequest\n";
+   #print Dumper($webRequest);
+   
+   if ($webRequest->is_success) {
+       $webHtml =  $webRequest->decoded_content;  # or whatever
+   }
+   else {
+       die $webRequest->status_line;
+   }
+   
+    
+   my @html = split(/\n/, $webHtml);
+   #print Dumper(@html);
+
+   my @sheet;
+   
+   foreach my $htmlLine (@html) {
+      chomp $htmlLine;
+   
+      if ($htmlLine =~ m#<div class="js-store" data-content="(.+)"></div>#) {
+         my $rawJson = $1;
+         $rawJson =~ s/&quot;/"/g;
+         #print "--> $rawJson\n";
+   
+         my @matches = ( $rawJson =~ m#(https://tabs.ultimate-guitar.com/tab/.+?)",#g );
+#        print Dumper(@matches);
+   
+         my $json = JSON->new->allow_nonref;
+   
+         my $jsonText = $json->decode( $rawJson );
+   
+         push @sheet,  @{ $jsonText->{'store'}->{'page'}->{'data'}->{'data'}->{'tabs'} };
+#        print Dumper($jsonText->{'store'}->{'page'}->{'data'}->{'data'});
+#        print Dumper(@sheet);
+   
+   
+         foreach my $tabHash (@sheet) {
+            print "processing $tabHash->{'artist_name'} / $tabHash->{'song_name'} ....\n";
+            getTab($tabHash);
+   
+         }
+   
+      }
+   
+   }
+   
+}
+
+
+exit 0;
+
+
+
+sub getTab
+{
+   my ($song_ref) = @_;
+
+   my $raw_url = $song_ref->{'tab_url'};
+
+   my $urlTab = URI::Heuristic::uf_urlstr($raw_url);
+   
+   my $uaTab = LWP::UserAgent->new();
+   $uaTab->agent($userAgent);
+   
+   my $requestTab = HTTP::Request->new(GET => $urlTab);
+   $requestTab->referer('https://www.ultimate-guitar.com/explore?type[]=Tabs');
+   #print "request = $request\n";
+   
+   my $webRequest = $uaTab->request($requestTab);
+   #print "web request = $webRequest\n";
+   #print Dumper($webRequest);
+   
+   if ($webRequest->is_success) {
+       $webHtml =  $webRequest->decoded_content;  # or whatever
+   }
+   else {
+       die $webRequest->status_line;
+   }
+   
+    
+   my @html = split(/\n/, $webHtml);
+   #print Dumper(@html);
+
+   foreach my $htmlLine (@html) {
+      chomp $htmlLine;
+   
+      if ($htmlLine =~ m#<div class="js-store" data-content="(.+)"></div>#) {
+         my $rawJson = $1;
+         $rawJson =~ s/&quot;/"/g;
+         #print "--> $rawJson\n";
+
+         my $json = JSON->new->allow_nonref;
+   
+         my $jsonText = $json->decode( $rawJson );
+         #print Dumper($jsonText);
+         #print "$jsonText->{'store'}->{'page'}->{'data'}->{'tab_view'}->{'wiki_tab'}->{'content'}\n";
+
+         my $artist = $song_ref->{'artist_name'};
+         $artist =~ s#/# #g;
+         decode_entities($artist);
+         my $song = $song_ref->{'song_name'};
+         decode_entities($song);
+         my $outName = "${artist}/${song}-$song_ref->{'type_name'}.txt";
+
+         mkdir "content/" unless (-e "content");
+         mkdir "content/$artist" unless (-e "content/$artist");
+
+         open (OUT, '>', "content/$outName") or warn "cannot write output (content/$outName) for write. $E\n";
+         (my $content = $jsonText->{'store'}->{'page'}->{'data'}->{'tab_view'}->{'wiki_tab'}->{'content'}) =~ s#\r##gm;
+         $content =~ s/\[.*tab\]//igm;
+         $content =~ s/^amp;/&/igm;
+         decode_entities($content);
+         print OUT "$content\n";
+         close OUT;
+
+         sleep 1;
+         return;
+      }
+
+   }
+
+   return;
+   
+}
+
+
